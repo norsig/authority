1020e67e32fb60476df8c7deb591bba7a776bbc8
Jeff Zellner <jeffz@eml.cc>
2015-09-12 13:14:53 -0600

    cli support for subject alt names

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"strings"
 
@@ -130,8 +131,24 @@ func (c *Client) GetConfig() error {
 // Generate creates and a certificate for the provided common name.
 // It will also generate and display a backend access token with granular
 // permissions to access the certificate.
-func (c *Client) Generate(name string, parent string) error {
-	_, token, err := c.api.GenerateWithParent(name, parent)
+func (c *Client) Generate(name string, parent string, dnsNames string, ipAddresses string) error {
+	var altNames []string
+	for _, name := range strings.Split(dnsNames, ",") {
+		trimmedName := strings.TrimSpace(name)
+		if trimmedName != "" {
+			altNames = append(altNames, trimmedName)
+		}
+	}
+
+	var altIPs []net.IP
+	for _, ipString := range strings.Split(ipAddresses, ",") {
+		ip := net.ParseIP(strings.TrimSpace(ipString))
+		if ip != nil {
+			altIPs = append(altIPs, ip)
+		}
+	}
+
+	_, token, err := c.api.GenerateWithOptions(name, parent, altNames, altIPs)
 	if err != nil {
 		return err
 	}
